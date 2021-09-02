d8079c52f321680b0d0ced7a5c160fc0d9fd8f06
cryptodeath <89591042+cryptodeath@users.noreply.github.com>
2021-09-02 12:39:09 +0800

    add mining

--- contracts/mining.sol
+++ contracts/mining.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./interface/INonfungiblePositionManager.sol";
@@ -15,23 +15,30 @@ contract Mining {
 
     Counters.Counter private _itemNum;
 
-    address tokenAddress; // contract address of reward erc20 token
-    address uniV3NFTAddress; // contract address of uniV3 NFT
+    address LPTokenAddress; // contract address of reward erc20 token
+    address rewardTokenAddress; // contract address of reward erc20 token
     address uniV3NFTManagerAddress; // contract addres of uniV3 NFT Manager
+    address uniV3NFTAddress; // contract address of uniV3 NFT
 
     // the reward range of this mining contract is [lowertick, uppertick]
     int24 upperTick;
     int24 lowerTick;
 
     uint256 lastRewardBlock;  // last block number that reward occurs
-    uint256 accCakePerShare; // Accumulated CAKEs per share, times 1e12. See below.
+    uint256 accTokenPerShare; // Accumulated Reward Tokens per share, times 1e12. See below.
 
     uint256 rewardPerBlock;
+    uint256 totalAmount;
+
+    // The block number when NFT mining starts.
+    uint256 public startBlock;
+    // The block number when NFT mining ends.
+    uint256 public endBlock;
 
     struct Item {
         address owner;
         uint256 tokenId;
-        bool retrieved;
+        bool exist;
     }
 
     mapping(uint256 => Item) private id2item;
@@ -40,17 +47,31 @@ contract Mining {
     event Deposit(address indexed user, uint256 tokenId, uint256 itemId);
     event Withdraw(address indexed user, uint256 tokenId, uint256 itemId);
 
-    constructor(address _tokenAddress, address _uniV3NFTAddress, address _token0, address _token1) {
-        require(_token0 != _token1);
-        (address token0, address token1) = _token0 < _token1 ? (_token0, _token1) : (_token1, _token0);
-        
-        tokenAddress = _tokenAddress;
-        uniV3NFTAddress = _uniV3NFTAddress;
+    constructor(
+        INonfungiblePositionManager _uniV3NFTManager,
+        IERC721 _uniV3NFT,
+        IERC20 _lpToken,
+        IERC20 _rewardToken,
+        uint256 _rewardPerBlock,
+        uint _startBlock,
+        uint _endBlock
+    ) {
+        uniV3NFTManager = _uniV3NFTManager;
+        uniV3NFT = _uniV3NFT;
+
+        lpToken = _lpToken;
+        rewardToken = _rewardToken;
+
+        rewardPerBlock = _rewardPerBlock;
+
+        startBlock = _startBlock;
+        endBlock = _endBlock;
+        lastRewardBlock = startBlock;
+        accTokenPerShare = 0;
+        totalAmount = 0;
     }
 
-    function getAmountForNFT(uint256 tokenId) public returns (uint256 amount) {
-        // the ID of the pool with which this token is connected
-        uint80 poolId;
+    function getAmountForNFT(uint256 tokenId) view public returns (uint256 amount) {
         // the tick range of the position
         int24 tickLower;
         int24 tickUpper;
@@ -62,35 +83,92 @@ contract Mining {
         return amount;
     }
 
-    function deposit(uint256 itemId) public returns (uint256 amount) {
-        require(id2item[itemId].owner == msg.sender, "");
-        IERC721(uniV3NFTAddress).transferFrom(msg.sender, address(this), id2item[itemId].tokenId);
-        IERC20(tokenAddress).transferFrom(address(this), msg.sender, amount);
+    // deposit NFT from user and returns according Reward tokens
+    function deposit(uint256 tokenId) public returns (uint256 amount) {
+        amount = getAmountForNFT(tokenId);
+        require(amount > 0, "INVALID NFT");
+
+        IERC721(uniV3NFTAddress).transferFrom(msg.sender, address(this), tokenId);
+        _itemNum.increment();
+        uint itemId = _itemNum.current();
+        id2item[itemId] = Item(
+            msg.sender,
+            tokenId,
+            true
+        );
+        
+        updatePool(amount);
+
+        IERC20(LPTokenAddress).transferFrom(address(this), msg.sender, amount);
         return amount;
     }
 
+    // withdraw NFT and returns according Reward tokens
     function withdraw(uint256 itemId) public {
+        require(id2item[itemId].owner == msg.sender, "NOT OWNER");
+        require(id2item[itemId].exist == true, "NOT EXIST");
 
+        amount = getAmountForNFT(id2item[itemId].tokenId);
+
+        updatePool(-amount);
+
+        IERC20(LPTokenAddress).transferFrom(msg.sender, address(this), amount);
         IERC721(uniV3NFTAddress).transferFrom(address(this), msg.sender, id2item[itemId].tokenId);
+        delete id2item[itemId];
     }
 
-    function pendingReward(address _user) external view returns (uint256) {
-        PoolInfo storage pool = poolInfo[0];
-        UserInfo storage user = userInfo[_user];
-        uint256 accCakePerShare = pool.accCakePerShare;
-        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
-        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
+    // Return reward multiplier over the given _from to _to block.
+    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
+        if (_to <= endBlock) {
+            return _to.sub(_from);
+        } else if (_from >= endBlock) {
+            return 0;
+        } else {
+            return endBlock.sub(_from);
+        }
+    }
+
+    // View function to see tokens from one user on frontend.
+    function getNFTIdsForUser(address _user) view external returns (uint[] tokenIds) {
+        uint[] tokenIds;
+        for (uint i = 0; i < _itemNum.current(); i++) {
+            if (id2item[i].retrieved) continue;
+            if (id2item[i].owner == _user) {
+                tokenIds.push(i);
+            }
+        }
+        return tokenIds;
+    }
+
+    // View function to see pending Reward on frontend.
+    function pendingReward(uint256 itemId) external view returns (uint256) {
+        if (block.number > pool.lastRewardBlock && totalAmount != 0) {
             uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-            uint256 cakeReward = multiplier.mul(rewardPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-            accCakePerShare = accCakePerShare.add(cakeReward.mul(1e12).div(lpSupply));
+            uint256 tokenReward = multiplier.mul(rewardPerBlock);
+            accTokenPerShare = accTokenPerShare.add(cakeReward.mul(1e12).div(totalAmount));
         }
         return user.amount.mul(accCakePerShare).div(1e12).sub(user.rewardDebt);
     }
 
+    // 
     function getReward(uint256 itemId) public {
 
     }
 
-    function getRewardForAll() public {
+    // Update reward variables to be up-to-date.
+    function updatePool(uint amount) public {
+        if (block.number <= lastRewardBlock) {
+            return;
+        }
+        if (totalAmount == 0) {
+            pool.lastRewardBlock = block.number;
+            return;
+        }
+        uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+        uint256 tokenReward = multiplier.mul(rewardPerBlock);
+        accTokenPerShare = accTokenPerShare.add(tokenReward.mul(1e12).div(totalAmount));
+        // update new state
+        totalAmount += amount;
+        lastRewardBlock = block.number;
     }
 }
