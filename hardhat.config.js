2ed04819fae812a21a31af96563015893b3d9001
cryptodeath <haha>
2021-09-07 22:40:46 +0800

    add univ3 deploy into hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("uniswap-v3-deploy-plugin");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +19,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    test: {
+      url: "http://0.0.0.0:9545",
+      accounts: ['00792d92886534d8153df0b5325ba974ba2af41736379ebba455208d5ac1f37a']
+    }
+  }
 };
