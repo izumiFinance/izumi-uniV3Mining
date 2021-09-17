b541688d9457552319f8c1a69e968270bc67f9db
MacRen000 <281130306@qq.com>
2021-09-17 15:28:41 +0800

    add debug scripts

---
+++ scripts/reRunTransaction.js
@@ -0,0 +1,25 @@
+const hardhat = require("hardhat");
+const web3 = require("web3")
+const config = require("../hardhat.config.js")
+
+const v = process.argv
+
+
+const para = {
+    network: process.env.HARDHAT_NETWORK,
+    rpc: config.networks[process.env.HARDHAT_NETWORK].url,
+    tx: v[2],
+}
+
+
+async function main() {
+    console.log("Paramters: ");
+    for ( var i in para) { console.log("    " + i + ": " + para[i]); }
+
+    const w3 = new web3(para.rpc);
+    const tx = await w3.eth.getTransaction(para.tx);
+    w3.eth.call(tx, tx.blockNumber);
+}
+
+
+main().then(()=>{}).catch((err)=>{console.log(err)})
