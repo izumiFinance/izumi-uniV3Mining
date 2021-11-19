6e2527036957966c7076ed763a43335d907ccc13
opcc <opcc22059@gmail.com>
2021-11-19 22:48:04 +0800

    add interface of postions in nfPositionManager

--- contracts/miningNoPermanentLoss/interfaces.sol
+++ contracts/miningNoPermanentLoss/interfaces.sol
@@ -44,7 +44,23 @@ interface INonfungiblePositionManager {
 
     /// @return Returns the address of WETH9
     function WETH9() external view returns (address);
-
+    function positions(uint256 tokenId)
+        external
+        view
+        returns (
+            uint96 nonce,
+            address operator,
+            address token0,
+            address token1,
+            uint24 fee,
+            int24 tickLower,
+            int24 tickUpper,
+            uint128 liquidity,
+            uint256 feeGrowthInside0LastX128,
+            uint256 feeGrowthInside1LastX128,
+            uint128 tokensOwed0,
+            uint128 tokensOwed1
+        );
     struct MintParams {
         address token0;
         address token1;
