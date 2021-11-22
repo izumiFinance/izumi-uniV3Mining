e58502ea0eb75921e972452b12b957ebd1620169
opcc <opcc22059@gmail.com>
2021-11-22 19:58:42 +0800

    rm useless var when query position from uniswap

--- contracts/miningOneSide/MiningOneSide.sol
+++ contracts/miningOneSide/MiningOneSide.sol
@@ -596,18 +596,18 @@ contract MiningOneSide is Ownable, Multicall {
         returns (PositionData memory posData)
     {
         (
-            uint96 nonce,
-            address operator,
+            ,
+            ,
             address token0,
             address token1,
             uint24 fee_,
             int24 tickLower,
             int24 tickUpper,
             uint128 liquidity,
-            uint256 feeGrowthInside0LastX128,
-            uint256 feeGrowthInside1LastX128,
-            uint128 tokensOwed0,
-            uint128 tokensOwed1
+            ,
+            ,
+            ,
+            
         ) = INonfungiblePositionManager(nftManager).positions(uniPositionID);
         posData.token0 = token0;
         posData.token1 = token1;
