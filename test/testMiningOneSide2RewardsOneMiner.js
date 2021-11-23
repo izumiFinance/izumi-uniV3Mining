f5fed1fb4dd037e6a7e24e35a916bd41a7a818b4
opcc <opcc22059@gmail.com>
2021-11-23 19:13:26 +0800

    simplify test case of mint with existing nft when tokenUni < tokenLock, and add test case of mining with nft when tokenUni > tokenLock

--- test/testMiningOneSide2RewardsOneMiner.js
+++ test/testMiningOneSide2RewardsOneMiner.js
@@ -434,35 +434,120 @@ describe("mining one side with 2 rewards", function () {
       await ethers.provider.send('evm_mine');
       await ethers.provider.send('evm_mine');
       await ethers.provider.send('evm_mine');
-      await mining.connect(miner1).collect("0", recipient1.address);
+
+      await mining.connect(miner1).withdraw(
+        "0",
+        recipient2.address,
+        "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+        false
+      );
+
+      await checkPositionOwner(uniPositionManager, posID, recipient2);
+
       var checkBlock1 = await ethers.provider.getBlockNumber();
+      var tokenReward0 = BigNumber(rewardInfo.tokenPerBlock0).times(checkBlock1 - checkBlock0);
+      var tokenReward1 = BigNumber(rewardInfo.tokenPerBlock1).times(checkBlock1 - checkBlock0);
 
-      var tokenReward0_01 = BigNumber(rewardInfo.tokenPerBlock0).times(checkBlock1 - checkBlock0);
-      var tokenReward1_01 = BigNumber(rewardInfo.tokenPerBlock1).times(checkBlock1 - checkBlock0);
-      var expectAccRewardPerShare0_01 = floor(tokenReward0_01.times(q128).div(totalVLiquidity)).toFixed(0);
-      var expectAccRewardPerShare1_01 = floor(tokenReward1_01.times(q128).div(totalVLiquidity)).toFixed(0);
+      var expectAccRewardPerShare0 = muldiv(tokenReward0, q128, BigNumber(totalVLiquidity)).toFixed(0);
+      var expectAccRewardPerShare1 = muldiv(tokenReward1, q128, BigNumber(totalVLiquidity)).toFixed(0);
 
-      var accRewardPerShare0_01 = (await mining.accRewardPerShare0()).toString();
-      var accRewardPerShare1_01 = (await mining.accRewardPerShare1()).toString();
-      expect(accRewardPerShare0_01).to.equal(expectAccRewardPerShare0_01);
-      expect(accRewardPerShare1_01).to.equal(expectAccRewardPerShare1_01);
-      // check reward of miner1 after collect
-      var expectMiner1Reward0_01 = muldiv(accRewardPerShare0_01, BigNumber(miner1VLiquidity), q128).toFixed(0);
-      var expectMiner1Reward1_01 = muldiv(accRewardPerShare1_01, BigNumber(miner1VLiquidity), q128).toFixed(0);
-      await checkBalance(token0, recipient1, expectMiner1Reward0_01);
-      await checkBalance(token1, recipient1, expectMiner1Reward1_01);
-      // check provider's balance
-      await checkBalance(token0, provider0, BigNumber("1000000000000000000000000").minus(expectMiner1Reward0_01).toFixed(0));
-      await checkBalance(token1, provider1, BigNumber("1000000000000000000000000").minus(expectMiner1Reward1_01).toFixed(0));
-      // check mining info after collect
+      var accRewardPerShare0 = (await mining.accRewardPerShare0()).toString();
+      var accRewardPerShare1 = (await mining.accRewardPerShare1()).toString();
+
+      expect(accRewardPerShare0).to.equal(expectAccRewardPerShare0);
+      expect(accRewardPerShare1).to.equal(expectAccRewardPerShare1);
+      var expectMiner1Reward0 = muldiv(accRewardPerShare0, BigNumber(miner1VLiquidity), q128).toFixed(0);
+      var expectMiner1Reward1 = muldiv(accRewardPerShare1, BigNumber(miner1VLiquidity), q128).toFixed(0);
+      await checkBalance(token0, recipient2, expectMiner1Reward0);
+      await checkBalance(token1, recipient2, expectMiner1Reward1);
+      await checkBalance(tokenY, recipient2, miner1AmountLockY);
+      // check mining info after withdraw
       expectMiningInfo = {
-        amountLock: "40000000000000000",
-        vLiquidity: "10000000000000000",
-        lastTouchAccRewardPerShare0: expectAccRewardPerShare0_01,
-        lastTouchAccRewardPerShare1: expectAccRewardPerShare1_01,
+        amountLock: "0",
+        vLiquidity: "0",
+        lastTouchAccRewardPerShare0: expectAccRewardPerShare0,
+        lastTouchAccRewardPerShare1: expectAccRewardPerShare1,
+        uniPositionID: "0",
+        isUniPositionIDExternal: true,
+      };
+      await checkMiningInfo(mining, "0", expectMiningInfo);
+      // check total vliquidity
+
+      totalVLiquidity = (await mining.totalVLiquidity()).toString();
+      expect(totalVLiquidity).to.equal("0");
+    });
+
+    it("check simply mining when tokenY is tokenUni, and mining with NFT", async function () {
+      
+      var tickLeft = -500040;
+      var tickRight = 13860;
+
+      var miner1AmountUniY = "10000000000000000";
+      var miner1AmountLockX = "2500000000000000";
+      var miner1VLiquidity = "10000000000000000";
+
+      var mintParam = {
+        token0: tokenX.address,
+        token1: tokenY.address,
+        fee: "3000",
+        tickLower: tickLeft,
+        tickUpper: tickRight,
+        amount0Desired: "0",
+        amount1Desired: miner1AmountUniY,
+        amount0Min: "0",
+        amount1Min: "1",
+        recipient: miner1.address,
+        deadline: "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+      }
+      await tokenY.transfer(miner1.address, "1000000000000000000");
+      await tokenY.connect(miner1).approve(uniPositionManager.address, miner1AmountUniY);
+      console.log("before uniswap mint");
+      await uniPositionManager.connect(miner1).mint(mintParam);
+      console.log("after uniswap mint");
+      var posID = "1";
+      await checkPositionOwner(uniPositionManager, posID, miner1);
+
+      const MiningFactory = await ethers.getContractFactory("MiningOneSide2Rewards");
+      console.log("before deploy");
+      mining = await MiningFactory.deploy(
+        tokenY.address,
+        tokenX.address,
+        "3000",
+        uniPositionManager.address,
+        rewardInfo,
+        startBlock,
+        endBlock
+      );
+      var token0 = await attachToken(rewardInfo.token0);
+      await token0.connect(provider0).approve(mining.address, "1000000000000000000000000");
+      var token1 = await attachToken(rewardInfo.token1);
+      await token1.connect(provider1).approve(mining.address, "1000000000000000000000000");
+
+      // miner1
+      await tokenX.transfer(miner1.address, "1000000000000000000");
+      await tokenX.connect(miner1).approve(mining.address, "40000000000000000");
+
+      await uniPositionManager.connect(miner1).approve(mining.address, posID);
+      // mint with existing nft
+      await mining.connect(miner1).mintWithExistingNFT(posID, "1");
+      await checkPositionOwner(uniPositionManager, posID, mining);
+      // check lock balance of contract
+      await checkBalance(tokenX, mining, miner1AmountLockX);
+      var checkBlock0 = await ethers.provider.getBlockNumber();
+
+      var expectTotalVLiquidity = miner1VLiquidity;
+      var totalVLiquidity = (await mining.totalVLiquidity()).toString();
+      expect(totalVLiquidity).to.equal(expectTotalVLiquidity);
+      // check mining info after mint
+      var expectMiningInfo = {
+        amountLock: miner1AmountLockX,
+        vLiquidity: miner1AmountUniY,
+        lastTouchAccRewardPerShare0: "0",
+        lastTouchAccRewardPerShare1: "0",
         uniPositionID: posID,
         isUniPositionIDExternal: true,
       };
+
       await checkMiningInfo(mining, "0", expectMiningInfo);
 
       await ethers.provider.send('evm_mine');
@@ -481,32 +566,29 @@ describe("mining one side with 2 rewards", function () {
 
       await checkPositionOwner(uniPositionManager, posID, recipient2);
 
-      var checkBlock2 = await ethers.provider.getBlockNumber();
-      var tokenReward0_12 = BigNumber(rewardInfo.tokenPerBlock0).times(checkBlock2 - checkBlock1);
-      var tokenReward1_12 = BigNumber(rewardInfo.tokenPerBlock1).times(checkBlock2 - checkBlock1);
+      var checkBlock1 = await ethers.provider.getBlockNumber();
+      var tokenReward0 = BigNumber(rewardInfo.tokenPerBlock0).times(checkBlock1 - checkBlock0);
+      var tokenReward1 = BigNumber(rewardInfo.tokenPerBlock1).times(checkBlock1 - checkBlock0);
 
-      var accRewardPerShare0_12 = muldiv(tokenReward0_12, q128, BigNumber(totalVLiquidity));
-      var expectAccRewardPerShare0_02 = accRewardPerShare0_12.plus(expectAccRewardPerShare0_01).toFixed(0);
+      var expectAccRewardPerShare0 = muldiv(tokenReward0, q128, BigNumber(totalVLiquidity)).toFixed(0);
+      var expectAccRewardPerShare1 = muldiv(tokenReward1, q128, BigNumber(totalVLiquidity)).toFixed(0);
 
-      var accRewardPerShare1_12 = muldiv(tokenReward1_12, q128, BigNumber(totalVLiquidity));
-      var expectAccRewardPerShare1_02 = accRewardPerShare1_12.plus(expectAccRewardPerShare1_01).toFixed(0);
+      var accRewardPerShare0 = (await mining.accRewardPerShare0()).toString();
+      var accRewardPerShare1 = (await mining.accRewardPerShare1()).toString();
 
-      var accRewardPerShare0_02 = (await mining.accRewardPerShare0()).toString();
-      var accRewardPerShare1_02 = (await mining.accRewardPerShare1()).toString();
-
-      expect(accRewardPerShare0_02).to.equal(expectAccRewardPerShare0_02);
-      expect(accRewardPerShare1_02).to.equal(expectAccRewardPerShare1_02);
-      var expectMiner1Reward0_12 = muldiv(accRewardPerShare0_12, BigNumber(miner1VLiquidity), q128).toFixed(0);
-      var expectMiner1Reward1_12 = muldiv(accRewardPerShare1_12, BigNumber(miner1VLiquidity), q128).toFixed(0);
-      await checkBalance(token0, recipient2, expectMiner1Reward0_12);
-      await checkBalance(token1, recipient2, expectMiner1Reward1_12);
-      await checkBalance(tokenY, recipient2, miner1AmountLockY);
+      expect(accRewardPerShare0).to.equal(expectAccRewardPerShare0);
+      expect(accRewardPerShare1).to.equal(expectAccRewardPerShare1);
+      var expectMiner1Reward0 = muldiv(accRewardPerShare0, BigNumber(miner1VLiquidity), q128).toFixed(0);
+      var expectMiner1Reward1 = muldiv(accRewardPerShare1, BigNumber(miner1VLiquidity), q128).toFixed(0);
+      await checkBalance(token0, recipient2, expectMiner1Reward0);
+      await checkBalance(token1, recipient2, expectMiner1Reward1);
+      await checkBalance(tokenX, recipient2, miner1AmountLockX);
       // check mining info after withdraw
       expectMiningInfo = {
         amountLock: "0",
         vLiquidity: "0",
-        lastTouchAccRewardPerShare0: expectAccRewardPerShare0_02,
-        lastTouchAccRewardPerShare1: expectAccRewardPerShare1_02,
+        lastTouchAccRewardPerShare0: expectAccRewardPerShare0,
+        lastTouchAccRewardPerShare1: expectAccRewardPerShare1,
         uniPositionID: "0",
         isUniPositionIDExternal: true,
       };
