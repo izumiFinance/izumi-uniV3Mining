bd187bb8a8e50b8b7cc63917074774a5a27cb824
opcc <opcc22059@gmail.com>
2021-12-20 18:08:05 +0800

    one side rm console.log

--- contracts/miningOneSideBoost/MiningOneSideBoost.sol
+++ contracts/miningOneSideBoost/MiningOneSideBoost.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.4;
 
 // Uncomment if needed.
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
