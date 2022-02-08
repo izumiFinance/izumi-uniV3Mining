ef0cfb3057af5d18ff360b0845e063546c540982
opcc <opcc22059@gmail.com>
2022-02-08 12:22:18 +0800

    update

---
+++ scripts/oneSide/constructArgs/ethUsdcDevt.js
@@ -0,0 +1,28 @@
+module.exports = [
+    {
+        uniV3NFTManager: '0xC36442b4a4522E871399CD717aBDD847Ab11FE88',
+        uniTokenAddr: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+        lockTokenAddr: '0xB5c578947de0fd71303F71F2C3d41767438bD0de',
+        fee: '3000'
+    },
+    [
+        {
+        rewardToken: '0x9ad37205d608B8b219e6a2573f922094CEc5c200',
+        provider: '0xaB11f1e579B177E80E7C20BE4cD8f4EcDF842A31',
+        accRewardPerShare: 0,
+        rewardPerBlock: '3240740000000000000'
+        },
+        {
+        rewardToken: '0xB5c578947de0fd71303F71F2C3d41767438bD0de',
+        provider: '0xaB11f1e579B177E80E7C20BE4cD8f4EcDF842A31',
+        accRewardPerShare: 0,
+        rewardPerBlock: '1080139000000000000'
+        }
+    ],
+    '1',
+    '0x9ad37205d608B8b219e6a2573f922094CEc5c200',
+    '14163662',
+    '14712919',
+    '40',
+    '0x3599A414B4365b118766479600c0fD135177C2D5'
+];
