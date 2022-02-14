68bcf50259d60847c1e12f166e872ede0f9696f5
opcc <opcc22059@gmail.com>
2022-02-14 13:35:14 +0800

    update

---
+++ scripts/oneSide/constructArgs/polygonEthYin.js
@@ -0,0 +1,28 @@
+module.exports = [
+    {
+      uniV3NFTManager: '0xC36442b4a4522E871399CD717aBDD847Ab11FE88',
+      uniTokenAddr: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
+      lockTokenAddr: '0x794Baab6b878467F93EF17e2f2851ce04E3E34C8',
+      fee: '3000'
+    },
+    [
+      {
+        rewardToken: '0x60d01ec2d5e98ac51c8b4cf84dfcce98d527c747',
+        provider: '0x4067B31B40e8eDb7487F999d34900C3102EeaD1a',
+        accRewardPerShare: 0,
+        rewardPerBlock: '231481481500000000'
+      },
+      {
+        rewardToken: '0x794Baab6b878467F93EF17e2f2851ce04E3E34C8',
+        provider: '0x4067B31B40e8eDb7487F999d34900C3102EeaD1a',
+        accRewardPerShare: 0,
+        rewardPerBlock: '57870370370000000'
+      }
+    ],
+    '1',
+    '0x60d01ec2d5e98ac51c8b4cf84dfcce98d527c747',
+    '24961451',
+    '26113451',
+    '40',
+    '0x3599A414B4365b118766479600c0fD135177C2D5'
+  ];
