d2f36792afb774a5abf0713f4ae05d650000dbf5
opcc <opcc22059@gmail.com>
2022-03-27 14:32:07 +0800

    update

---
+++ scripts/fixRange/constructArgs/arbitrumUSDCUSDT-3-28.js
@@ -0,0 +1,29 @@
+module.exports = [
+    {
+        uniV3NFTManager: '0xC36442b4a4522E871399CD717aBDD847Ab11FE88',
+        token0: '0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9',
+        token1: '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8',
+        fee: '500'
+    },
+    [
+        {
+            rewardToken: '0x60D01EC2D5E98Ac51C8B4cF84DfCCE98D527c747',
+            provider: '0x5B0bb2e0A6B0cab625d885DfBe95fC67E50E5F3c',
+            accRewardPerShare: 0,
+            rewardPerBlock: '3240740741000000000'
+        },
+        {
+            rewardToken: '0x794Baab6b878467F93EF17e2f2851ce04E3E34C8',
+            provider: '0x5B0bb2e0A6B0cab625d885DfBe95fC67E50E5F3c',
+            accRewardPerShare: 0,
+            rewardPerBlock: '648148148100000000'
+        }
+    ],
+    '0x60D01EC2D5E98Ac51C8B4cF84DfCCE98D527c747',
+    50,
+    0,
+    '14472669',
+    '14663983',
+    '40',
+    '0x3599A414B4365b118766479600c0fD135177C2D5'
+];
